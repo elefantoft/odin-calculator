ca00c95b2692ab56a19b9b05575d03c85a241dad
Kjetil <KjetilAlvestad@gmail.com>
2022-03-07 14:59:16 +0100

    Add keyboard support

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # odin-calculator
 
 This project is part of The Odin Projects foundations course. In it, I'll be making a calculator using what I've learned about HTML, CSS and JavaScript. 
+
+The calculator is functional, but it has some bugs if you press buttons in a random order.
+I will come back and fix these bugs, and add a GUI, at a later date.
--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 const digits = document.querySelectorAll('.digits button');
 const operator = document.querySelectorAll('.operators button');
-const equals = document.querySelector('#equals');
-const clear = document.querySelector('#clear');
-const decimal = document.querySelector('#decimal');
+const equals = document.querySelector('.equals');
+const clear = document.querySelector('.clear');
+const decimal = document.querySelector('.decimal');
 const output = document.querySelector('.output');
-const clearEntry = document.querySelector('#CE');
+const clearEntry = document.querySelector('.CE');
+
 let operatorNum = 0;
 let decimalClicked = false;
 let decimalNumber = false;
+let equaled = false;
+let operatorChecked = false;
+
+let input = {
+    digit: [...''],
+    operator: [...''],
+    sum: []
+};
 
 const add = function(...theArgs) {
     let sum = theArgs[0];
@@ -57,50 +66,8 @@ const operate = function(a, b, c) {
     };
 };
 
-let input = {
-    digit: [...''],
-    operator: [...''],
-    sum: []
-};
-
-digits.forEach((button) => {
-        button.addEventListener('click', () => {{
-            if (decimalClicked && decimalNumber) {
-            input.digit[operatorNum] += `${button.textContent}`;
-            input.digit[operatorNum] = input.digit[operatorNum].split("undefined").pop();
-            output.textContent = input.digit[operatorNum];
-            decimalNumber = false;
-            } else if (!decimalNumber && decimalClicked) {
-                return;
-            } else {
-            input.digit[operatorNum] += `${button.textContent}`;
-            input.digit[operatorNum] = input.digit[operatorNum].split("undefined").pop();
-            output.textContent = input.digit[operatorNum];
-            }
-            }
-})
-});
-
-decimal.addEventListener('click', () => {
-    if (!decimalClicked) {
-    input.digit[operatorNum] += `.`;
-    output.textContent = input.digit[operatorNum];
-    decimalClicked = true;
-    decimalNumber = true;
-    } else {return};
-});
-
-operator.forEach((button) => {
-    button.addEventListener('click', () => {
-        input.operator[operatorNum] += `${button.textContent}`;
-        input.operator[operatorNum] = input.operator[operatorNum].split("undefined").pop();
-        sum();
-        (operatorNum > 0) ? output.textContent = input.sum : false;
-        operatorNum++;
-    });
-});
-
-let sum = function() {
+const sum = function() {
+    if (!equaled) {
     if (operatorNum < 2) {
         input.sum = operate(input.operator[0], parseFloat(input.digit[0]), parseFloat(input.digit[1]));
     } else {
@@ -109,28 +76,124 @@ let sum = function() {
     input.sum = Math.round(input.sum * 10) / 10;
     decimalClicked = false;
     decimalNumber = false;
+    operatorChecked = false;
     return input.sum;
 };
+};
 
-equals.addEventListener('click', () => {
+const equate = function() {
     if (typeof input.digit[1] === 'undefined') {
         return;
     };
     input.sum = sum();
     output.textContent = input.sum;
+    equaled = true;
     return input.sum;
-});
+};
 
-clear.addEventListener('click', () => {
+const digitInput = function(variable) {
+    if (decimalClicked && decimalNumber) {
+        input.digit[operatorNum] += `${variable.textContent}`;
+        input.digit[operatorNum] = input.digit[operatorNum].split("undefined").pop();
+        output.textContent = input.digit[operatorNum];
+        decimalNumber = false;
+        operatorChecked = false;
+        equaled = false;
+        } else if (!decimalNumber && decimalClicked) {
+            return;
+        } else {
+        input.digit[operatorNum] += `${variable.textContent}`;
+        input.digit[operatorNum] = input.digit[operatorNum].split("undefined").pop();
+        output.textContent = input.digit[operatorNum];
+        equaled = false;
+        operatorChecked = false;
+}};
+
+const operatorInput = function(variable) {
+    if (!operatorChecked) {
+    input.operator[operatorNum] += `${variable.textContent}`;
+    input.operator[operatorNum] = input.operator[operatorNum].split("undefined").pop();
+    sum();
+    (operatorNum > 0) ? output.textContent = input.sum : false;
+    operatorNum++;
+    operatorChecked = true;
+    };
+};
+
+const addDecimal = function() {
+    if (!decimalClicked) {
+        input.digit[operatorNum] += `.`;
+        output.textContent = input.digit[operatorNum];
+        decimalClicked = true;
+        decimalNumber = true;
+        } else {return};
+};
+
+const CE = function() {
+    if (input.digit[operatorNum]) {
+    input.digit[operatorNum] = input.digit[operatorNum].slice(0, -1);
+    input.digit[operatorNum] = input.digit[operatorNum].split("undefined").pop();
+    output.textContent = input.digit[operatorNum];
+    } else {return;};
+};
+
+const reset = function() {
     output.textContent = 0;
     operatorNum = 0;
     input.digit = [...''];
     input.operator = [...''];
     input.sum = [];
+    decimalClicked = false;
+    decimalNumber = false;
+    equaled = false;
+    operatorChecked = false;
+};
+
+
+digits.forEach((button) => {
+    button.addEventListener('click', () => {{
+        digitInput(button);
+        }
+})
+});
+
+operator.forEach((button) => {
+    button.addEventListener('click', () => {
+        operatorInput(button);
+    });
+});
+
+decimal.addEventListener('click', () => {
+    addDecimal();
+});
+
+equals.addEventListener('click', () => {
+    equate();
+});
+
+clear.addEventListener('click', () => {
+    reset();
 });
 
 clearEntry.addEventListener('click', () => {
-    input.digit[operatorNum] = input.digit[operatorNum].slice(0, -1);
-    input.digit[operatorNum] = input.digit[operatorNum].split("undefined").pop();
-    output.textContent = input.digit[operatorNum];
+    CE();
+});
+
+window.addEventListener('keydown', function(e) {    
+    const key = document.querySelector(`button[data-key="${e.keyCode}"]`);
+    if (!key) {
+        return;
+    } else if (key.parentElement.classList.contains('digits')) {
+        digitInput(key);
+    } else if (key.parentElement.classList.contains('operators')) {
+        operatorInput(key);
+    } else if (key.classList.contains('equals')) {
+        equate();
+    } else if (key.classList.contains('decimal')) {
+        addDecimal();
+    } else if (key.classList.contains('CE')) {
+        CE();
+    } else if (key.classList.contains('clear')) {
+        reset();
+    };
 });
